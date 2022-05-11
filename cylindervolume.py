0abcb5f9a28dde2890a1e52b37a0918607f268f1
Dineshkumar Sankar <dk021617@gmail.com>
2022-05-11 19:55:12 +0530

    .

--- cylindervolume.py
+++
@@ -1,4 +0,0 @@
-radius = int(input("Enter the radius of cylinder:"))
-height = int(input("Enter the height of cylinder:"))
-volume = 3.14*(radius**2)*height
-print("Volume of the cylinder is:",round(volume,2))
