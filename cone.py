58af03cd30b469717dea475b169ece835f68c4a3
Dineshkumar Sankar <dk021617@gmail.com>
2022-05-11 20:00:29 +0530

    M10 added

--- cone.py
+++ cone.py
@@ -1,4 +1,4 @@
 radius = int(input("Enter the radius of cone:"))
 height = int(input("Enter the height of cone:"))
-volume = 3.14*(radius**2)*(h/3)
+volume = 3.14*(radius**2)*(height/3)
 print("Volume of the cone is:",round(volume,2))
