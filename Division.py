3e556b688b69e7551ac81ddcb2304cf0709adfcd
R Arunraj <75235747+arunraj2002@users.noreply.github.com>
2022-05-12 23:22:24 +0530

    M3 Added

---
+++ Division.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[17]:
+
+
+import math
+a=int(input("Enter the first number: "))
+b=int(input("Enter the second number: "))
+d=a/b
+print("The division of {0} and {1} is: {2}".format(a,b,d))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
