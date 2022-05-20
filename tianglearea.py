a33c4932946f73a52d79a597fcefcf6d2b3d7a6e
Arunakash-ai <70016683+Arunakash-ai@users.noreply.github.com>
2022-05-20 12:43:29 +0530

    Add files via upload

---
+++ tianglearea.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+h = int(input("Enter the height:"))
+b = int(input("E@nter the breadth:"))
+tarea = 0.5*h*b
+print("Area of the triangle:",tarea)
+
+
+# In[ ]:
+
+
+
+
